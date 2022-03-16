2e43bc75b20efd29b364b3fb2fc98f6cdaf0e982
bakkka <91085270+bakkka@users.noreply.github.com>
2022-03-16 15:40:59 +0300

    Update files.cpp

--- files.cpp
+++ files.cpp
@@ -10,6 +10,7 @@
 #include <conio.h>
 #include <locale.h>
 using namespace std;
+
 struct Dict {
     string name;
     Dict* adr;
@@ -107,11 +108,13 @@ void pole(int yc, int xc)
     cout << "ENTER-ОТКРЫТЬ ПАПКУ" << endl;
     cout << "ESC-ПРОСМОТР ФАЙЛА" << endl;
     cout << "BACKSPACE-ВЕРНУТЬСЯ НАЗАД" << endl;
+    cout << "ВЫБЕРИТЕ ФАЙЛ ДЛЯ СРАВНЕНИЕ И НАЖМИТЕ ПРОБЕЛ";
     cout << "\n\n";
 }
 int main()
 {
     setlocale(LC_ALL, "RUSSIAN");
+   
     bool exit = true;
     char vibor = 1;
     int x = 1, y = 1;
@@ -124,7 +127,10 @@ int main()
     const int enter = 13;
     const int esc = 27;
     const int backspace=8;
+    const int tab = 9;
+    const int space = 32;
     int raz = 0;
+    int kol = 0;
     int viv =1;
     string strh;
     string str;
@@ -135,9 +141,13 @@ int main()
     gotoxy(x, y);
     vivod(cr,1);
     int strelka = 55;
+    char* papka = new char;
+    
     while (exit) {     
         gotoxy(strelka, vibor);
-        printf("<==");
+        if (raz == 0) {
+            printf("<==");
+        }
         code_simvola = _getch();
         if (code_simvola == 224)
             code_simvola = _getch();
@@ -151,6 +161,7 @@ int main()
             cout << "   ";
             break;
         case enter:          
+                raz = 0;
                 system("cls");
                 pole(visota + 2, xx);
                 cr = spisok();
@@ -164,10 +175,13 @@ int main()
                 pole(visota + 2, xx);              
                 cout << "ОТКРЫТАЯ ПАПКА:"<<cr->file.name;
                 cr = spisok();
+              
                 vivod(cr, 1);
             break;
-        case backspace:           
+        case backspace:
             _chdir("..\\");
+            raz = 0;
+            
             visota = dlina();
             system("cls");
             pole(visota+2, xx);
@@ -175,26 +189,108 @@ int main()
             vivod(cr,1);          
             break;
         case esc:
+            if (raz == 0) {
+                raz = 1;
                 str = "";
+                system("cls");
+                
+                for (int a = 0; a < vibor - 1; a++) {
+                    cr = cr->adr;
+                }
+                ifstream fin(cr->file.name);
+                if (fin.is_open()) {
+                    while (!fin.eof()) {
+                        getline(fin, strh);
+                        str += strh;
+                        str += ' ';
+                        if (!fin.eof())
+                            str += "\n";
+                    }
+
+                    cout << str;
+                  
+                    _chdir(cr->file.name);
+                    fin.close();
+                }
+            }
+            break;
+        case tab:
+            
+            gotoxy(0, visota + 6);
+            cout << "Введите имя папки, которую хотите создать: ";    
+            cin >> papka;
+            _mkdir(papka);
+            system("cls");
+            visota = dlina();
+            pole(visota + 2, xx);
+            cr = spisok();
+            vivod(cr, 1);
+            break;
+        case space:
+            string ch1, ch2, chh1, chh2;
+            ifstream fin1, fin2;
+            bool result = true;
+            ch1 = "";
+            ch2 = "";
+            if (kol == 0) {
+                for (int a = 0; a < vibor - 1; a++) {
+                        cr = cr->adr;
+                }
+                ifstream fin1(cr->file.name,ios::binary);
+                kol++;     
+                visota = dlina();
                 system("cls");
                 pole(visota + 2, xx);
                 cr = spisok();
                 vivod(cr, 1);
+                break;               
+            }
+            else {            
                 for (int a = 0; a < vibor - 1; a++) {
                     cr = cr->adr;
                 }
-                char buff[65];
-                    ifstream fin(cr->file.name);                  
-                        if (fin.is_open()) {
-                            while (!fin.eof()) {
-                                getline(fin, strh);
-                                str += strh;
-                                str += ' ';
-                            }
-                            gotoxy(66, 1);
-                            cout << str;
-                            fin.close();
-                        }               
+                ifstream fin2(cr->file.name,ios::binary);               
+                kol = 0;
+            }
+            while (!fin1.eof()) {
+                getline(fin1, chh1);
+                ch1 += chh1;
+                ch1 += ' ';
+                if (!fin1.eof())
+                    ch1 += "\n";
+            }
+            while (!fin2.eof()) {
+                getline(fin2, chh2);
+                ch2 += chh2;
+                ch2 += ' ';
+                if (!fin2.eof())
+                    ch2 += "\n";
+            }
+            if (ch1 != ch2) {
+                 result = false;                       
+                 break;
+            }
+                
+            
+            if (result == true) {
+                system("cls");
+                visota = dlina();
+                pole(visota + 2, xx);
+                cr = spisok();
+                vivod(cr, 1);
+                gotoxy(0, visota + 9);
+                cout << "ОДИНАКОВЫЕ ФАЙЛЫ";
+                
+            }
+            else {
+                system("cls");
+                visota = dlina();
+                pole(visota + 2, xx);
+                cr = spisok();
+                vivod(cr, 1);
+                gotoxy(0, visota + 9);
+                cout << "РАЗНЫЕ ФАЙЛЫ";
+            }
             break;
         }
         if (vibor < 1)
